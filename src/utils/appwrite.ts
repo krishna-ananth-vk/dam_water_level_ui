547c6339381e25058bed9d50f4eb4c5dd703a377
Ananthakrishnan VK <krishnaananthvk@gmail.com>
2024-08-09 04:35:00 +0530

    changed function id

--- src/utils/appwrite.ts
+++ src/utils/appwrite.ts
@@ -4,7 +4,7 @@ export const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(process.env.PROJECT_ID||'dam-water-level');
+    .setProject(process.env.PROJECT_ID||'');
 
 export const functions = new Functions(client);
 
@@ -12,7 +12,7 @@ export const account = new Account(client);
 export { ID } from 'appwrite';
 
 
-export const getBearerTokenFunctionId =  '66b4662e001492a5d503';
+export const getBearerTokenFunctionId = process.env.GET_BEARER_TOKEN_FUNCTION_ID || '';
 
-export const OLA_MAPS_API_KEY = process.env.OLA_MAPS_API_KEY;
+export const OLA_MAPS_API_KEY = process.env.OLA_MAPS_API_KEY || '';
 
